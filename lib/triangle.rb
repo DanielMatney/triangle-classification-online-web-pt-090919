83ac0f30bc81676301942c4a2c8813da43d9ed77
Daniel Matney <danieljmatney@gmail.com>
2019-10-22 19:21:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     if @side1 == @side2 || @side1 == @side3 || @side2 == @side3
       return :isosceles
     end
-    if @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
+    if @side1 + @side2 < @side3 || @side1 + @side3 < @side2 || @side2 + @side3 < @side1
       raise TriangleError
     end
     return :scalene
